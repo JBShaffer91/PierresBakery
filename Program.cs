ca2c742b9e5bc817ba5155adb3bef0c073c0273f
Justin Shaffer <justinbshaffer91@gmail.com>
2023-04-20 10:17:03 -0700

    Fix issues in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,37 +3,32 @@ using PierresBakery.Models;
 
 namespace PierresBakery
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      DisplayWelcomeMessage();
+        static void Main(string[] args)
+        {
+            DisplayWelcomeMessage();
+
+            Console.Write("Enter the number of loaves of bread: ");
+            int breadQuantity = int.Parse(Console.ReadLine());
+            Bread bread = new Bread(breadQuantity);
+
+            Console.Write("Enter the number of pastries: ");
+            int pastryQuantity = int.Parse(Console.ReadLine());
+            Pastry pastry = new Pastry(pastryQuantity);
+
+            int breadCost = bread.CalculatePrice();
+            int pastryCost = pastry.CalculatePrice();
+            int totalCost = breadCost + pastryCost;
+
+            Console.WriteLine($"Total cost of your order: ${totalCost}");
+        }
+
+        static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+            Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
+        }
     }
-
-    static void DisplayWelcomeMessage()
-    {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("We sell bread and pastries.");
-      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
-      Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
-    }
-
-    static void Main(string[] args)
-    {
-      DisplayWelcomeMessage();
-
-      Console.Write("How many loaves of bread would you like? ");
-      int breadQuantity = int.Parse(Console.ReadLine());
-
-      Console.Write("How many pastries would you like? ");
-      int pastryQuantity = int.Parse(Console.ReadLine());
-      Pastry pastry = new Pastry(pastryQuantity);
-
-      int breadCost = bread.CalculatePrice();
-      int pastryCost = pastry.CalculatePrice();
-      int totalCost = breadCost + pastryCost;
-
-      Console.WriteLine("Your total is $" + totalCost);
-    }
-  }
 }
