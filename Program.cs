971af146b677ce4cca488aa2635f2f50fda5bf93
Justin Shaffer <justinbshaffer91@gmail.com>
2023-04-20 10:14:05 -0700

    Implement console user interface

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using PierresBakery.Models;
+
+namespace PierresBakery
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      DisplayWelcomeMessage();
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We sell bread and pastries.");
+      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
+    }
+
+    static void Main(string[] args)
+    {
+      DisplayWelcomeMessage();
+
+      Console.Write("How many loaves of bread would you like? ");
+      int breadQuantity = int.Parse(Console.ReadLine());
+
+      Console.Write("How many pastries would you like? ");
+      int pastryQuantity = int.Parse(Console.ReadLine());
+      Pastry pastry = new Pastry(pastryQuantity);
+
+      int breadCost = bread.CalculatePrice();
+      int pastryCost = pastry.CalculatePrice();
+      int totalCost = breadCost + pastryCost;
+
+      Console.WriteLine("Your total is $" + totalCost);
+    }
+  }
+}
