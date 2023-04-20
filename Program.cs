48c7785189762cd8e76d67f24be2bcd2fc573586
Justin Shaffer <justinbshaffer91@gmail.com>
2023-04-20 11:03:54 -0700

    Resolve null warning in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,32 +3,45 @@ using PierresBakery.Models;
 
 namespace PierresBakery
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            DisplayWelcomeMessage();
+      int breadQuantity = GetUserInput("How many loaves of bread would you like?");
+      Bread bread = new Bread(breadQuantity);
 
-            Console.Write("Enter the number of loaves of bread: ");
-            int breadQuantity = int.Parse(Console.ReadLine());
-            Bread bread = new Bread(breadQuantity);
+      int pastryQuantity = GetUserInput("How many pastries would you like?");
+      Pastry pastry = new Pastry(pastryQuantity);
 
-            Console.Write("Enter the number of pastries: ");
-            int pastryQuantity = int.Parse(Console.ReadLine());
-            Pastry pastry = new Pastry(pastryQuantity);
+      int breadPrice = bread.CalculatePrice();
+      int pastryPrice = pastry.CalculatePrice();
+      int totalPrice = breadPrice + pastryPrice;
 
-            int breadCost = bread.CalculatePrice();
-            int pastryCost = pastry.CalculatePrice();
-            int totalCost = breadCost + pastryCost;
-
-            Console.WriteLine($"Total cost of your order: ${totalCost}");
-        }
-
-        static void DisplayWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to Pierre's Bakery!");
-            Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
-            Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
-        }
+      Console.WriteLine($"Your total is ${totalPrice}.");
     }
+
+    static void DisplayWelcomeMessage()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
+    }
+
+    static int GetUserInput(string prompt)
+    {
+      int result;
+      string input;
+      Console.Write(prompt);
+      input = Console.ReadLine() ?? "";
+
+      while (!int.TryParse(input, out result) || result < 0)
+       {
+         Console.WriteLine("Invalid input. Please enter a non-negative number.");
+         Console.Write(prompt);
+         input = Console.ReadLine() ?? "";
+        }
+
+      return result;
+    }
+  }
 }
